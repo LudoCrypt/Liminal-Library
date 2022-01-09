d627d2de4459b5d6770008cc387317b02e63ac03
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-09 06:40:16 -0600

    Update ChunkStatusMixin.java

--- src/main/java/net/ludocrypt/limlib/mixin/ChunkStatusMixin.java
+++ src/main/java/net/ludocrypt/limlib/mixin/ChunkStatusMixin.java
@@ -7,8 +7,8 @@ import java.util.function.Function;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -20,17 +20,31 @@ import net.minecraft.server.world.ServerLightingProvider;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.world.ChunkRegion;
+import net.minecraft.world.chunk.BelowZeroRetrogen;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.ProtoChunk;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 
 @Mixin(ChunkStatus.class)
 public class ChunkStatusMixin {
 
 	@Inject(method = "method_38284(Lnet/minecraft/world/chunk/ChunkStatus;Ljava/util/concurrent/Executor;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/structure/StructureManager;Lnet/minecraft/server/world/ServerLightingProvider;Ljava/util/function/Function;Ljava/util/List;Lnet/minecraft/world/chunk/Chunk;Z)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "RETURN", ordinal = 0, shift = Shift.BEFORE), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-	private static void limlib$noise(ChunkStatus targetStatus, Executor executor, ServerWorld world, ChunkGenerator generator, StructureManager structureManager, ServerLightingProvider lightingProvider, Function<Chunk, CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> function, List<Chunk> chunks, Chunk chunk2, boolean bl, CallbackInfoReturnable<CompletableFuture<Chunk>> ci, ChunkRegion chunkRegion) {
+	private static void limlib$noise(ChunkStatus targetStatus, Executor executor, ServerWorld world, ChunkGenerator generator, StructureManager structureManager, ServerLightingProvider lightingProvider, Function<Chunk, CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> function, List<Chunk> chunks, Chunk chunk2, boolean bl, CallbackInfoReturnable<CompletableFuture<?>> ci, ChunkRegion chunkRegion) {
 		if (generator instanceof NbtChunkGenerator nbtChunkGenerator) {
-			ci.setReturnValue(((NbtChunkGenerator) generator).populateNoise(executor, chunk2, targetStatus, world, new ChunkRegion(world, chunks, targetStatus, nbtChunkGenerator.getChunkRadius()), structureManager, lightingProvider));
+			ci.setReturnValue(((NbtChunkGenerator) generator).populateNoise(executor, chunk2, targetStatus, world, new ChunkRegion(world, chunks, targetStatus, nbtChunkGenerator.getChunkRadius()), structureManager, lightingProvider).thenApply(chunk -> {
+				if (chunk instanceof ProtoChunk protoChunk) {
+					BelowZeroRetrogen belowZeroRetrogen = protoChunk.getBelowZeroRetrogen();
+					if (belowZeroRetrogen != null) {
+						BelowZeroRetrogen.replaceOldBedrock(protoChunk);
+						if (belowZeroRetrogen.hasMissingBedrock()) {
+							belowZeroRetrogen.fillColumnsWithAirIfMissingBedrock(protoChunk);
+						}
+					}
+					protoChunk.setStatus(targetStatus);
+				}
+				return Either.left(chunk);
+			}));
 		}
 	}
 
