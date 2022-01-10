d682a3e7818537807fbcb8ea8259f11698d56014
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-10 17:35:01 -0600

    Update IMPL class

--- src/main/java/net/ludocrypt/limlib/api/sound/LiminalTravelSound.java
+++ src/main/java/net/ludocrypt/limlib/api/sound/LiminalTravelSound.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang3.mutable.Mutable;
 
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 
 public abstract class LiminalTravelSound {
 
@@ -17,15 +19,19 @@ public abstract class LiminalTravelSound {
 
 	public static class SimpleTravelSound extends LiminalTravelSound {
 
-		private SoundEvent sound;
+		private final RegistryKey<World> world;
+		private final SoundEvent sound;
 
-		public SimpleTravelSound(SoundEvent sound) {
+		public SimpleTravelSound(RegistryKey<World> world, SoundEvent sound) {
+			this.world = world;
 			this.sound = sound;
 		}
 
 		@Override
 		public void hookSound(ServerWorld from, ServerWorld to, Mutable<Optional<SoundEvent>> mutable) {
-			mutable.setValue(Optional.of(sound));
+			if (to.getRegistryKey().equals(world)) {
+				mutable.setValue(Optional.of(sound));
+			}
 		}
 
 	}
