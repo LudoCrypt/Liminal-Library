d992e00746815bf6acbe516a9ee053a1ae7268a8
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-11 15:29:07 -0600

    Update dimensionEffectsMixin

--- src/main/java/net/ludocrypt/limlib/mixin/DimensionEffectsMixin.java
+++ src/main/java/net/ludocrypt/limlib/mixin/DimensionEffectsMixin.java
@@ -1,26 +1,28 @@
 package net.ludocrypt.limlib.mixin;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.ludocrypt.limlib.impl.render.LiminalDimensionEffects;
 import net.minecraft.client.render.DimensionEffects;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.dimension.DimensionType;
 
 @Environment(EnvType.CLIENT)
 @Mixin(DimensionEffects.class)
 public class DimensionEffectsMixin {
 
-	@Shadow
-	@Final
-	private static Object2ObjectMap<Identifier, DimensionEffects> BY_IDENTIFIER;
-
-	static {
-		LiminalDimensionEffects.DIMENSION_EFFECTS_REGISTRY.forEach((key, effects) -> BY_IDENTIFIER.put(key.getValue(), effects));
+	@Inject(method = "byDimensionType", at = @At("HEAD"), cancellable = true)
+	private static void limlib$byDimensionType(DimensionType dimensionType, CallbackInfoReturnable<DimensionEffects> ci) {
+		DimensionEffects limlibEffect = LiminalDimensionEffects.DIMENSION_EFFECTS_REGISTRY.get(RegistryKey.of(Registry.WORLD_KEY, dimensionType.getEffects()));
+		if (limlibEffect != null) {
+			ci.setReturnValue(limlibEffect);
+		}
 	}
 
 }
