a0c52779aaf59ca3981c172d0a9ea791af5f481a
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-11 18:18:13 -0600

    update priorities

--- src/main/java/net/ludocrypt/limlib/impl/sound/LiminalTravelSounds.java
+++ src/main/java/net/ludocrypt/limlib/impl/sound/LiminalTravelSounds.java
@@ -26,7 +26,7 @@ public class LiminalTravelSounds {
 	public static Optional<SoundEvent> getCurrent(ServerWorld from, ServerWorld to) {
 		MutableObject<Optional<SoundEvent>> mutableSound = new MutableObject<Optional<SoundEvent>>(Optional.of(SoundEvents.BLOCK_PORTAL_TRAVEL));
 		List<LiminalTravelSound> list = Lists.newArrayList(TRAVEL_SOUND_REGISTRY.values());
-		Collections.sort(list, (a, b) -> Integer.compare(b.priority(), a.priority()));
+		Collections.sort(list, (a, b) -> Integer.compare(a.priority(), b.priority()));
 		for (LiminalTravelSound sound : list) {
 			sound.hookSound(from, to, mutableSound);
 		}
