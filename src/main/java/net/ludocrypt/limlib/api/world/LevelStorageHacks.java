9a9f193cb8bba0888702c09b103c7c808f1767da
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-13 14:27:45 -0600

    Level Storage Mixin Attempt 1

---
+++ src/main/java/net/ludocrypt/limlib/api/world/LevelStorageHacks.java
@@ -0,0 +1,13 @@
+package net.ludocrypt.limlib.api.world;
+
+import net.minecraft.util.registry.DynamicRegistryManager;
+
+@Deprecated
+public class LevelStorageHacks {
+
+	// TODO: This whole class is just for me to cheat at getting what i want!
+	// Dont mind this~
+
+	public static DynamicRegistryManager.Impl earlyDynamicRegistryManager = null;
+
+}
---
+++ src/main/java/net/ludocrypt/limlib/mixin/LevelStorageMixin.java
@@ -0,0 +1,42 @@
+package net.ludocrypt.limlib.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import com.mojang.datafixers.DataFixer;
+import com.mojang.serialization.Dynamic;
+
+import net.ludocrypt.limlib.api.world.LevelStorageHacks;
+import net.ludocrypt.limlib.api.world.LiminalWorld;
+import net.ludocrypt.limlib.impl.world.LiminalDimensions;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.level.storage.LevelStorage;
+
+@SuppressWarnings("deprecation")
+@Mixin(LevelStorage.class)
+public class LevelStorageMixin {
+
+	@ModifyVariable(method = "readGeneratorProperties", at = @At(value = "STORE"), ordinal = 1)
+	private static <T> Dynamic<T> limlib$readGeneratorProperties$datafix(Dynamic<T> in, Dynamic<T> levelData, DataFixer dataFixer, int version) {
+		Dynamic<T> dynamic = in;
+		if (LevelStorageHacks.earlyDynamicRegistryManager != null) {
+			LiminalDimensions.LIMINAL_WORLD_REGISTRY.forEach((world) -> limlib$addDimension(world, in, LevelStorageHacks.earlyDynamicRegistryManager));
+		}
+		return dynamic;
+	}
+
+	@Unique
+	private static <T> Dynamic<T> limlib$addDimension(LiminalWorld world, Dynamic<T> in, DynamicRegistryManager.Impl registry) {
+		System.out.println("Adding");
+		Dynamic<T> dimensions = in.get("dimensions").orElseEmptyMap();
+		if (!dimensions.get(world.worldId.toString()).result().isPresent()) {
+			dimensions.set(world.worldId.toString(), new Dynamic<T>(dimensions.getOps(), DimensionOptions.CODEC.encodeStart(dimensions.getOps(), world.worldDimensionOptions.apply(registry.get(Registry.DIMENSION_TYPE_KEY), registry.get(Registry.BIOME_KEY), in.get("seed").asLong(0))).result().get()));
+		}
+		return in;
+	}
+
+}
