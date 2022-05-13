bb1832676801c4e8fc68b040b8f1d9eefe2a18d7
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-05-13 14:31:24 -0500

    Deprecated Maze Generation

---
+++ src/main/java/net/ludocrypt/limlib/api/world/maze/deprecated/MazeGenerator.java
@@ -0,0 +1,92 @@
+package net.ludocrypt.limlib.api.world.maze.deprecated;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.function.Function;
+
+import com.mojang.serialization.Codec;
+
+import net.ludocrypt.limlib.api.world.maze.deprecated.MazeComponent.CellState;
+import net.ludocrypt.limlib.impl.LimlibRegistries;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ChunkRegion;
+import net.minecraft.world.chunk.Chunk;
+
+@Deprecated
+public abstract class MazeGenerator {
+
+	public static final Codec<MazeGenerator> CODEC = LimlibRegistries.LIMINAL_MAZE_GENERATOR.getCodec().dispatchStable(MazeGenerator::getCodec, Function.identity());
+
+	private final HashMap<BlockPos, MazeComponent> mazes = new HashMap<BlockPos, MazeComponent>(30);
+
+	public final int width;
+	public final int height;
+	public final int thickness;
+	public final boolean redundancy;
+	public final long seedModifier;
+
+	public MazeGenerator(int width, int height, int thickness, boolean redundancy, long seedModifier) {
+		this.width = width;
+		this.height = height;
+		this.thickness = thickness;
+		this.redundancy = redundancy;
+		this.seedModifier = seedModifier;
+	}
+
+	public void generateMaze(BlockPos pos, Chunk chunk, ChunkRegion region) {
+		if (thickness < 1)
+			throw new UnsupportedOperationException("Thickness can not be less than 1");
+
+		for (int x = 0; x < 16; x++) {
+			for (int y = 0; y < 16; y++) {
+				BlockPos inPos = pos.add(x, 0, y);
+				if (mod(inPos.getX(), thickness) == 0 && mod(inPos.getZ(), thickness) == 0) {
+					BlockPos mazePos = new BlockPos(inPos.getX() - mod(inPos.getX(), (width * thickness * 2)), 0, inPos.getZ() - mod(inPos.getZ(), (height * thickness * 2)));
+
+					MazeComponent maze;
+					if (this.mazes.containsKey(mazePos)) {
+						maze = this.mazes.get(mazePos);
+					} else {
+						maze = this.newMaze(region, chunk, redundancy ? width + 4 : width, redundancy ? height + 4 : height, new Random(blockSeed(mazePos.getX(), mazePos.getZ(), seedModifier)));
+						this.mazes.put(mazePos, maze);
+					}
+
+					BlockPos originPos = new BlockPos(inPos.getX() - mod(inPos.getX(), (2 * thickness)), 0, inPos.getZ() - mod(inPos.getZ(), (2 * thickness)));
+
+					boolean isOpenCell = mod(inPos.getX(), (2 * thickness)) == 0 && mod(inPos.getZ(), (2 * thickness)) == 0;
+					boolean isWallCell = mod(inPos.getX(), (2 * thickness)) == thickness && mod(inPos.getZ(), (2 * thickness)) == thickness;
+					boolean isTopCell = mod(inPos.getX(), (2 * thickness)) == thickness && mod(inPos.getZ(), (2 * thickness)) == 0;
+					boolean isSideCell = mod(inPos.getX(), (2 * thickness)) == 0 && mod(inPos.getZ(), (2 * thickness)) == thickness;
+
+					int mazeX = mod(originPos.getX(), (width * thickness * 2)) / (2 * thickness);
+					int mazeY = mod(originPos.getZ(), (height * thickness * 2)) / (2 * thickness);
+
+					CellState originCell = maze.cellState(redundancy ? mazeX + 2 : mazeX, redundancy ? mazeY + 2 : mazeX);
+
+					this.decorateCell(inPos, originPos, chunk, region, originCell, isOpenCell, isWallCell, isTopCell, isSideCell, thickness);
+				}
+			}
+		}
+	}
+
+	public abstract MazeComponent newMaze(ChunkRegion region, Chunk chunk, int width, int height, Random random);
+
+	public abstract void decorateCell(BlockPos pos, BlockPos origin, Chunk chunk, ChunkRegion region, CellState state, boolean isOpen, boolean isWall, boolean isTopCell, boolean isSideCell, int thickness);
+
+	public abstract Codec<? extends MazeGenerator> getCodec();
+
+	protected int mod(int x, int n) {
+		int r = x % n;
+		if (r < 0) {
+			r += n;
+		}
+		return r;
+	}
+
+	protected long blockSeed(long x, long y, long z) {
+		long l = (x * 3129871) ^ z * 116129781L ^ y;
+		l = l * l * 42317861L + l * 11L;
+		return l >> 16;
+	}
+
+}
--- src/main/java/net/ludocrypt/limlib/impl/LimlibRegistries.java
+++ src/main/java/net/ludocrypt/limlib/impl/LimlibRegistries.java
@@ -8,10 +8,11 @@ import net.ludocrypt.limlib.api.LiminalWorld;
 import net.ludocrypt.limlib.api.render.LiminalBaseEffects;
 import net.ludocrypt.limlib.api.render.LiminalShaderApplier;
 import net.ludocrypt.limlib.api.render.LiminalSkyRenderer;
+import net.ludocrypt.limlib.api.world.maze.deprecated.MazeGenerator;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.SimpleRegistry;
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings({ "unchecked", "deprecation" })
 public class LimlibRegistries {
 
 	public static final SimpleRegistry<LiminalWorld> LIMINAL_WORLD = FabricRegistryBuilder.createSimple(LiminalWorld.class, new Identifier("limlib", "limlib_world")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
@@ -19,5 +20,7 @@ public class LimlibRegistries {
 	public static final SimpleRegistry<Codec<? extends LiminalSkyRenderer>> LIMINAL_SKY_RENDERER = (SimpleRegistry<Codec<? extends LiminalSkyRenderer>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier("limlib", "limlib_sky_renderer")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
 	public static final SimpleRegistry<Codec<? extends LiminalShaderApplier>> LIMINAL_SHADER_APPLIER = (SimpleRegistry<Codec<? extends LiminalShaderApplier>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier("limlib", "limlib_shader_applier")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
 	public static final SimpleRegistry<Codec<? extends LiminalBaseEffects>> LIMINAL_BASE_EFFECTS = (SimpleRegistry<Codec<? extends LiminalBaseEffects>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier("limlib", "limlib_base_effects")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
+	public static final SimpleRegistry<Codec<? extends MazeGenerator>> LIMINAL_MAZE_GENERATOR = (SimpleRegistry<Codec<? extends MazeGenerator>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier("limlib", "limlib_maze_generator")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
 
 }
