53b25babe569537b819a7b71a03204f4426239ae
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2023-10-11 19:02:11 -0500

    Update mappings

--- src/main/java/net/ludocrypt/limlib/impl/mixin/WorldDimensionsMixin.java
+++ src/main/java/net/ludocrypt/limlib/impl/mixin/WorldDimensionsMixin.java
@@ -16,23 +16,23 @@ import com.google.common.collect.Sets;
 import net.ludocrypt.limlib.api.LimlibWorld;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.unmapped.C_oiwekzxo;
 import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.WorldDimensions;
 
-@Mixin(C_oiwekzxo.class)
-public class DimensionOptionsC_oiwekzxoMixin {
+@Mixin(WorldDimensions.class)
+public class WorldDimensionsMixin {
 
 	@Shadow
 	@Final
 	@Mutable
-	private static Set<RegistryKey<DimensionOptions>> field_40364;
+	private static Set<RegistryKey<DimensionOptions>> VANILLA_DIMENSION_KEYS;
 
 	@Inject(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/util/Set;size()I", shift = Shift.BEFORE, ordinal = 0))
 	private static void limlib$clinit(CallbackInfo ci) {
 		Set<RegistryKey<DimensionOptions>> dimensions = Sets.newHashSet();
-		dimensions.addAll(field_40364);
+		dimensions.addAll(VANILLA_DIMENSION_KEYS);
 		LimlibWorld.LIMLIB_WORLD.getEntries().forEach((entry) -> dimensions.add(RegistryKey.of(RegistryKeys.DIMENSION, entry.getKey().getValue())));
-		field_40364 = dimensions;
+		VANILLA_DIMENSION_KEYS = dimensions;
 	}
 
 }
